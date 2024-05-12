c8c3f0da6c33aa38ef6f35ad47fb84f4f11d5416
Siddhartha <siddharthapaul101@gmail.com>
2024-05-12 12:41:36 +0530

    initial repo setup

---
+++ src/main/java/com/user/EmsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsUserServiceApplication.class, args);
+	}
+
+}
